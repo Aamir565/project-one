569ab494b3f661dcf94bb04b4831b46113bc86d4
Aamir Rayeen <aamir.rayeen.565@gmail.com>
2024-01-31 22:30:57 +0530

    add database

--- config/db.js
+++ config/db.js
@@ -1,3 +1,8 @@
 const mongoose = require("mongoose");
-const connectURL = "mongodb://0.0.0.0:27017/reactDB";
+// const connectURL = "mongodb://0.0.0.0:27017/reactDB";
+const connectURL = "mongodb+srv://aamirrayeen565:DuGVXlKCjuYbrQGz@cluster0.wl52tjw.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(connectURL);
+
+
+//username = aamirrayeen565
+//password = DuGVXlKCjuYbrQGz
